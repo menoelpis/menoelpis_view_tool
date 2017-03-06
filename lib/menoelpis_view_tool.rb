2022ea174993e4968724eb8c090af71f70b0c61f
Daniel Park <menoelpis@gmail.com>
2017-03-06 18:14:28 +0900

    Updated gitignore and README

---
+++ lib/menoelpis_view_tool.rb
@@ -0,0 +1,6 @@
+require "menoelpis_view_tool/version"
+require "menoelpis_view_tool/renderer"
+
+module MenoelpisViewTool
+  # Your code goes here...
+end
---
+++ lib/menoelpis_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MenoelpisViewTool
+  VERSION = "0.1.0"
+end
